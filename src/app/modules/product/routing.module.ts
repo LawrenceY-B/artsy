28d10ebf997287618d2c7a290e839e90b3dcca8c
Lawrence Yirenkyi-Boafo <lawrencekyb@gmail.com>
2023-05-13 22:33:55 +0000

    .

--- src/app/modules/product/routing.module.ts
+++ src/app/modules/product/routing.module.ts
@@ -10,7 +10,7 @@ import { ProductComponent } from './product.component';
 const routes: Routes = [
   { path: '', component: ProductComponent, children : [
     {
-      path: 'home',
+      path: '',
       component: HomeComponent,
       title: 'Home Page'
     } ,
@@ -25,7 +25,7 @@ const routes: Routes = [
       title: 'Home details'
     }
   ] },
-  { path: '',   redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home',   redirectTo: '', pathMatch: 'full'},
 
 
 ];
