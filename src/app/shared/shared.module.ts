c8fde00bce53e28298f77a1cfe57150107395238
Lawrence Yirenkyi-Boafo <lawrencekyb@gmail.com>
2023-07-08 16:52:13 +0000

    minor changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExhibitionCardComponent } from './libs/exhibition-card/exhibition-card.component';
 import { NavCardComponent } from './libs/nav-card/nav-card.component';
+import { FooterComponent } from './libs/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     ExhibitionCardComponent,
-    NavCardComponent
+    NavCardComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     ExhibitionCardComponent,
-    NavCardComponent
-
+    NavCardComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
