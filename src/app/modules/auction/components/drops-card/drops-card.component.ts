c50d0feb3656509a476cb7d9594355c70332e2ef
Lawrence Yirenkyi-Boafo <lawrencekyb@gmail.com>
2023-09-20 15:54:01 +0000

    drops✅

--- src/app/modules/auction/components/drops-card/drops-card.component.ts
+++ src/app/modules/auction/components/drops-card/drops-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IAuctionData } from 'src/app/shared/model/auction.model';
 
 @Component({
   selector: 'app-drops-card',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drops-card.component.scss']
 })
 export class DropsCardComponent implements OnInit {
-
+@Input() result!:IAuctionData
   constructor() { }
 
   ngOnInit(): void {
--- src/app/modules/auction/pages/drops/drops.component.ts
+++ src/app/modules/auction/pages/drops/drops.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { IAuctionData } from 'src/app/shared/model/auction.model';
+import { AuctionService } from 'src/app/shared/services/auction.service';
 
 @Component({
   selector: 'app-drops',
   templateUrl: './drops.component.html',
-  styleUrls: ['./drops.component.scss']
+  styleUrls: ['./drops.component.scss'],
 })
 export class DropsComponent implements OnInit {
-  notification:Boolean = false;
-  constructor() { }
+  notification: Boolean = false;
 
+  constructor(private AuctionService: AuctionService) {}
+  AuctionData: IAuctionData[] = [];
   ngOnInit(): void {
+    this.fetchData()
   }
-  notify(x:HTMLImageElement){
+  fetchData() {
+    this.AuctionService.getAuctionData().subscribe((data) => {
+      this.AuctionData = data.auctiondata;
+      console.table(this.AuctionData)
+    });
+  }
+  notify(x: HTMLImageElement) {
     this.notification = !this.notification;
-    if(this.notification){
-      x.src="assets/svgs/bell-tick.svg"
+    if (this.notification) {
+      x.src = 'assets/svgs/bell-tick.svg';
       this.notification = true;
-    }
-    else if(!this.notification ){
-      x.src="assets/svgs/bell-outline.svg"
+    } else if (!this.notification) {
+      x.src = 'assets/svgs/bell-outline.svg';
     }
   }
-
 }
